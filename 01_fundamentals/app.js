b704f8402fc1649d114cd19e160548b3854899c5
Richard Gonzalez <rgonzal92@gmail.com>
2022-07-09 18:51:43 -0500

    console commands

--- 01_fundamentals/app.js
+++ 01_fundamentals/app.js
@@ -1,1 +1,16 @@
-alert('Hello World!');
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+console.error('Error!');
+console.clear();
+console.warn('Warning!');
+console.time('Hello');
+console.timeEnd('Hello');
